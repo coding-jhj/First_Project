d168fb172419d00c60f0608e688f0e5bc4b36eb3
JeongHwanJu <ghkswnwjd@gmail.com>
2026-04-30 12:26:47 +0900

    fix: BoundingBoxOverlay BuildConfig import 추가

--- android/app/src/main/java/com/voiceguide/BoundingBoxOverlay.kt
+++ android/app/src/main/java/com/voiceguide/BoundingBoxOverlay.kt
@@ -7,6 +7,7 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import com.voiceguide.BuildConfig
 
 /**
  * 바운드박스 오버레이 View.
